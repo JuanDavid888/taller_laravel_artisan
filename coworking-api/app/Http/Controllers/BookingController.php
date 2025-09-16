d26b71f8d89fd9d80611c65b537919da63ee99bf
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-09-16 14:15:39 +0000

    feat: :construction: Metodo update de booking en progreso
    Se modificaron los archivos y hace su labo, pero retorna un mensaje desconocido

--- coworking-api/app/Http/Controllers/BookingController.php
+++ coworking-api/app/Http/Controllers/BookingController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreBookingRequest;
 use App\Http\Requests\UpdateBookingRequest;
+use App\Traits\ApiResponse;
 use App\Models\Booking;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
+    use ApiResponse;
     /**
      * Display a listing of the resource.
      */
@@ -17,22 +23,13 @@ class BookingController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreBookingRequest $request) {
-        $data = $request->validated();
         // TODO: validar no-solapamiento aquí o con Rule custom
-        $booking = Booking::create($data);
+        $booking = Booking::create($request->validated());
         return response()->json($booking, 201);
-      }
+    }
 
     /**
      * Display the specified resource.
@@ -43,20 +40,25 @@ class BookingController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Booking $booking)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(UpdateBookingRequest $request, Booking $booking)
     {
-        //
+        // Registra la solicitud y archivos para depuración
+        Log::debug('all:', $request->all());
+        Log::debug('files:', array_keys($request->allFiles()));
+
+        // Obtiene los datos validados
+        $data = $request->validated();
+
+        // Actualiza la reserva con los nuevos datos
+        $booking->update($data);
+
+        // Carga relaciones necesarias y devuelve la respuesta
+        $booking->load(['user', 'categories']);
+        return $this->success(new UpdateBookingRequest());
     }
+
 
     /**
      * Remove the specified resource from storage.
